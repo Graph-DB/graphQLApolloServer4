555f1618f054e1352045f62125aa4ffb7ebc1ed6
Wayne Mareci <Wayne@Mareci.com>
2024-08-27 15:55:21 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import BusinessResults from "./BusinessResults";
+import { gql, useQuery } from "@apollo/client";
+import { useAuth0 } from "@auth0/auth0-react";
+import "./App.css";
+import Profile from "./Profile";
+
+/*
+const businesses: BusinessInterface[] = [
+  {
+    businessId: "b1",
+    name: "San Mateo Public Library",
+    address: "55 W 3rd Ave",
+    category: "Library",
+  },
+  {
+    businessId: "b2",
+    name: "Ducky's Car Wash",
+    address: "716 N San Mateo Dr",
+    category: "Car Wash",
+  },
+  {
+    businessId: "b3",
+    name: "Hanabi",
+    address: "723 California Dr",
+    category: "Restaurant",
+  },
+];
+*/
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
+  const BUSINESS_DETAILS_FRAGMENT = gql`
+    fragment businessDetails on Business {
+      businessId
+      name
+      address
+      categories {
+        name
+      }
+    }
+  `;
+  const GET_BUSINESSES_QUERY = gql`
+  query BusinessesByCategory($selectedCategory: String!) {
+    businesses(
+      where: { categories_SOME: { name_CONTAINS: $selectedCategory } }
+    ) {
+      ...businessDetails
+      ${isAuthenticated ? "averageStars" : ""}
+      isStarred @client
+    }
+  }
+  ${BUSINESS_DETAILS_FRAGMENT}
+`;
+
+  const { loading, error, data, refetch } = useQuery(GET_BUSINESSES_QUERY, {
+    variables: { selectedCategory },
+    //pollInterval: 500
+  });
+
+  if (error) return <p>Error: {error.message.toString()}</p>;
+  if (loading) return <p>Loading...</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!isAuthenticated && (
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+      )}
+      {isAuthenticated && <button onClick={() => logout()}>Log Out</button>}
+      <Profile />
+      <h1>Business Search</h1>
+      <form>
+        <label>
+          Select Business Category:
+          <select
+            value={selectedCategory}
+            onChange={(event) => setSelectedCategory(event.target.value)}
+          >
+            <option value="">All</option>
+            <option value="Library">Library</option>
+            <option value="Restaurant">Restaurant</option>
+            <option value="Car Wash">Car Wash</option>
+          </select>
+        </label>
+        {/*<input type="submit" value="Submit" />*/}
+        <input type="button" value="Refetch" onClick={() => refetch()} />
+      </form>
+
+      <BusinessResults
+        businesses={
+          data.businesses
+          /*
+          selectedCategory === "All"
+            ? businesses
+            : businesses.filter((b) => {
+                return b.category === selectedCategory;
+              })
+                */
+        }
+      />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,91 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  makeVar,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+
+export const starredVar = makeVar([]);
+
+const AppWithApollo = () => {
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0()
+
+  const httpLink = createHttpLink({
+    uri: "http://localhost:4000"
+  })
+
+  const authLink = setContext(async (_, { headers }) => {
+    // only try to fetch access token if user is authenticated
+    const accessToken = isAuthenticated ? await getAccessTokenSilently() : undefined
+    if (accessToken) {
+      //console.log("accessToken: " + accessToken)
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}`:""
+        }
+      }
+    } else {
+      return {
+        headers: {
+          ...headers
+        }
+      }
+    }
+  })
+
+  const client = new ApolloClient({
+    //uri: "http://localhost:4000",
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Business: {
+          fields: {
+            isStarred: {
+              read(_, { readField }) {
+                return starredVar().includes(readField("businessId") as never);
+              },
+            },
+          },
+        },
+      },
+    }),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  )
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN as string}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID as string}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <AppWithApollo />
+      {/*<ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+      */}
+    </Auth0Provider>
   </React.StrictMode>
 );
 
